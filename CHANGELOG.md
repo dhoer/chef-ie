3e4347881be7cf64213484b4d70e6d6e36274ca2
Dennis Hoer <dennis.hoer@pearson.com>
2015-03-31 09:33:32 -0700

    Fixes #2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.0.2
+
+- Update ie_version to support older versions
+
 ## 1.0.1
 
 - Do not set group policy in security zones recipe
--- libraries/default.rb
+++ libraries/default.rb
@@ -1,7 +1,12 @@
+# http://support.microsoft.com/en-us/kb/969393
 def ie_version
   return unless platform_family?('windows')
-  registry_get_values('HKLM\SOFTWARE\Microsoft\Internet Explorer').each do |value|
-    return value[:data] if value[:name] == 'svcVersion'
+  values = registry_get_values('HKLM\SOFTWARE\Microsoft\Internet Explorer')
+  values.each do |value|
+    return value[:data] if value[:name] == 'svcVersion' # ie >= 10
+  end
+  values.each do |value|
+    return value[:data] if value[:name] == 'Version' # ie < 10
   end
   ''
 end
