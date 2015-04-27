a1a1f6696649e74b3aab7a078ffde615011257e6
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-27 10:30:26 -0700

    Remove unnecessary loop

--- libraries/default.rb
+++ libraries/default.rb
@@ -1,12 +1,10 @@
-# http://support.microsoft.com/en-us/kb/969393
 def ie_version
   return unless platform_family?('windows')
-  values = registry_get_values('HKLM\SOFTWARE\Microsoft\Internet Explorer')
-  values.each do |value|
+  version = ''
+  registry_get_values('HKLM\SOFTWARE\Microsoft\Internet Explorer').each do |value|
     return value[:data] if value[:name] == 'svcVersion' # ie >= 10
+    # http://support.microsoft.com/en-us/kb/969393
+    version = value[:data] if value[:name] == 'Version' # ie < 10
   end
-  values.each do |value|
-    return value[:data] if value[:name] == 'Version' # ie < 10
-  end
-  ''
+  version
 end
